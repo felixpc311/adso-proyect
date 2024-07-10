5a3e71ccabb926c20b0430623706d1079d34049d
felixpc311 <felixpuello311@gmail.com>
2024-07-10 13:10:33 -0500

    Card con props

--- adso-project-front/src/components/Card.jsx
+++ adso-project-front/src/components/Card.jsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom"
 import imagen from '../assets/pexels-mirrographer-1164572.jpg'
 import "../styles/Card.css"
 
-export default function Card() {
+export default function Card({titulo, descripcion, url, pie}) {
   return (
     <div className="tarjeta">
         <div className="titulo">
-            Colegio
+            {titulo}
         </div>
         <div className="cuerpo">
             <img src={imagen} alt="a school" />
-            Descripcion
+            {descripcion}
         </div>
         <div className="foot">
-            <Link href="#">Mas informacion</Link>
+            <Link to={url}>{pie}</Link>
         </div>
     </div>
   )
--- adso-project-front/src/components/Home.jsx
+++ adso-project-front/src/components/Home.jsx
@@ -1,7 +1,8 @@
 import Card from "./Card";
 
 export default function Home() {
+  const miCard = {titulo: "Mi Colegio", descripcion: "Ubicado en Monteria, Cordoba", url: "https://ieantoniasantossince.edu.co/", pie: "Más informacion"}
   return (
-    <Card />
+    <Card{...miCard} />
   )
 }
